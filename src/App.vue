67df6818136697544b817dc6da2f0c4369a89998
wssds <ssean.gerigk@gmx.net>
2024-02-10 19:12:58 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import NavBarComp from './components/NavBarComp.vue';
+</script>
+
+<template>
+  <div class="c-app__wrapper">
+    <NavBarComp />
+    <RouterView />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+@media only screen and (min-width: 600px) {
+.c-app__wrapper {
+  display: flex;
+}
+}
+</style>
---
+++ src/components/NavBarComp.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+    <nav class="c-navbar">
+
+    </nav>
+</template>
+
+<style scoped lang="scss">
+
+.c-navbar {
+    height: 30px;
+    background-color: brown;
+}
+
+@media only screen and (min-width: 600px) {
+.c-navbar {
+    width: 50px;
+    height: 100vh;
+    background-color: brown;
+}
+}
+</style>
